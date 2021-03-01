27d05aab130ec8f29d52d704bc0ceb57d165db5d
muhammadferdiansyah <dandyferdiansyah2801@gmail.com>
2021-03-01 10:16:21 +0700

    Initial Commit

---
+++ Food App/Food_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_AppApp.swift
+//  Food App
+//
+//  Created by Dandy Ferdiansyah on 01/03/21.
+//
+
+import SwiftUI
+
+@main
+struct Food_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
